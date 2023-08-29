76fcc79f3ea8f54815b8106af7e305c1858f3679
MirellaRossi <rm97990@fiap.com.br>
2023-08-29 11:51:43 -0300

    rodape

--- src/components/rodape.jsx
+++ src/components/rodape.jsx
@@ -1,17 +1,20 @@
+import { createElement } from "react";
+
 export default function Rodape(props) {
-    return (
-      <>
-        <footer>
-          
-        <ul>
-            {props.listaProps.map( (li)=>{
-                return {li};
-        })}
-        </ul>
-  
-          <p>&copy; 2021, Vite.js and React.</p>
-        </footer>
-      </>
-    );
-  }
+  return (
+    <>
+      <footer>
+        
+      <ul>
+          {props.listaProps.map( (li,index)=>{
+            createElement('li', key={index}, li);
+              return li;
+      })}
+      </ul>
+
+        <p>&copy; 2021, Vite.js and React.</p>
+      </footer>
+    </>
+  );
+}
   
